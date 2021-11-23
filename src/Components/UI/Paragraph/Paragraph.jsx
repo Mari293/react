4bdea0b1c901be1fe9ee23a25bb10d87109d925d
mari293 <maricruzg020903@gmail.com>
2021-11-22 23:28:22 -0500

    Se hace footer

--- src/Components/UI/Paragraph/Paragraph.jsx
+++ src/Components/UI/Paragraph/Paragraph.jsx
@@ -5,7 +5,7 @@ export const Paragraph = () => {
         <>
             <div className="information">
                 <h2>Recordemos algunas de las características de React</h2> 
-                <p>Es una librería de Javascript<br></br>
+                <p className="information-text">Es una librería de Javascript<br></br>
                 Funciona basado en un elemento conocido como el virtual DOM<br></br>
                 Renderiza solamente los componentes que necesita, lo que hace que sea más versátil<br></br>
                 La utilización de los hooks facilita su lógica frontend
